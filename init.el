283c8fd86a29fed2d1b53992990a281f649f7ff7
rigidus <i.am.rigidus@gmail.com>
2014-04-10 13:14:08 +0400

    lp

--- init.el
+++ init.el
@@ -1448,6 +1448,45 @@ to the previously saved position"
                       (setq indent-tabs-mode nil))))
 
 
+;;
+;; Org Babel
+;;
+(require 'ob-tangle)
+(setq org-ditaa-jar-path "/usr/share/ditaa/ditaa.jar")
+;; (setq org-plantuml-jar-path "~/java/plantuml.jar")
+
+
+(defun bh/display-inline-images ()
+  (condition-case nil
+      (org-display-inline-images)
+    (error nil)))
+(add-hook 'org-babel-after-execute-hook 'bh/display-inline-images 'append)
+
+;; Make babel results blocks lowercase
+(setq org-babel-results-keyword "results")
+
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (dot . t)
+         (ditaa . t)
+         (R . t)
+         (python . t)
+         (ruby . t)
+         (gnuplot . t)
+         (clojure . t)
+         (sh . t)
+         (ledger . t)
+         (org . t)
+         (plantuml . t)
+         (latex . t))))
+
+; Do not prompt to confirm evaluation
+; This may be dangerous - make sure you understand the consequences
+; of setting this -- see the docstring for details
+;; (setq org-confirm-babel-evaluate nil)
+
+
 ;; FORMATTING
 
 ;; abbrev-mode
