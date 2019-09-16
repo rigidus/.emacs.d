f5f052e25485e9f06de40c936371e58529b71015
rigidus <avenger-f@yandex.ru>
2019-09-16 07:27:21 +0300

    company-mode & company-flx

--- init.el
+++ init.el
@@ -967,6 +967,50 @@ Version 2018-10-05"
 (setq erc-max-buffer-size 500000)
 
 
+
+;; COMPANY-MODE
+
+;; Once installed, enable company-mode with M-x company-mode.
+
+;; Completion will start automatically after you type a few letters.
+;; Use:
+;; M-n and M-p to select,
+;;  <return> to complete or
+;; <tab> to complete the common part.
+;; Search through the completions with C-s, C-r and C-o.
+;; Press M-(digit) to quickly complete with one of the first 10 candidates.
+
+;; Type M-x company-complete to initiate completion manually.
+;; Bind this command to a key combination of your choice.
+
+;; When the completion candidates are shown,
+;; press <f1> to display the documentation for the selected candidate,
+;; or C-w to see its source. Not all back-ends support this.
+
+;; To use company-mode in all buffers, add the following line to your init file:
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; To see or change the list of enabled back-ends,
+;; type M-x customize-variable RET company-backends.
+;; Also see its description for information on writing a back-end.
+;; For information on specific back-ends,
+;; also check out the comments inside the respective files.
+;; For more information, type M-x describe-function RET company-mode.
+;; To customize other aspects of its behavior,
+;; type M-x customize-group RET company.
+
+
+
+
+;; COMPANY-FLX
+
+(with-eval-after-load 'company
+  (company-flx-mode +1))
+
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; OrgMode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
