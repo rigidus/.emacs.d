352ad463eb14df88e2336e237e2e1f56f5339bbb
rigidus <avenger-f@yandex.ru>
2020-07-27 13:34:48 +0300

    gofmt-before-save disabled by errors

--- init.el
+++ init.el
@@ -1430,19 +1430,19 @@ Version 2018-10-05"
 ;; (require 'go-eldoc)
 ;; (require 'company-go)
 
-(add-hook 'before-save-hook 'gofmt-before-save)
-(setq-default gofmt-command "goimports")
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-(add-hook 'go-mode-hook
-          (lambda ()
-            (set (make-local-variable 'company-backends) '(company-go))
-            (company-mode)))
-(add-hook 'go-mode-hook 'yas-minor-mode)
-;; (add-hook 'go-mode-hook 'flycheck-mode)
-(setq multi-compile-alist
-      '((go-mode
-         . (("go-build" "go build -v"
-             (locate-dominating-file buffer-file-name ".git"))
-            ("go-build-and-run"
-             "go build -v && echo 'build finish' && eval ./${PWD##*/}"
-             (multi-compile-locate-file-dir ".git"))))))
+;; (add-hook 'before-save-hook 'gofmt-before-save)
+;; (setq-default gofmt-command "goimports")
+;; (add-hook 'go-mode-hook 'go-eldoc-setup)
+;; (add-hook 'go-mode-hook
+;;           (lambda ()
+;;             (set (make-local-variable 'company-backends) '(company-go))
+;;             (company-mode)))
+;; (add-hook 'go-mode-hook 'yas-minor-mode)
+;; ;; (add-hook 'go-mode-hook 'flycheck-mode)
+;; (setq multi-compile-alist
+;;       '((go-mode
+;;          . (("go-build" "go build -v"
+;;              (locate-dominating-file buffer-file-name ".git"))
+;;             ("go-build-and-run"
+;;              "go build -v && echo 'build finish' && eval ./${PWD##*/}"
+;;              (multi-compile-locate-file-dir ".git"))))))
